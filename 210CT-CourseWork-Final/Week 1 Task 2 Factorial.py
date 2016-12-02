d21b5561a44f6d048d5a47cbbaab9cc4fa67b0c0
SirCodesABunch <patric16@uni.coventry.ac.uk>
2016-12-02 18:46:47 +0000

    Add files via upload
    Works now.

--- 210CT-CourseWork-Final/Week 1 Task 2 Factorial.py
+++ 210CT-CourseWork-Final/Week 1 Task 2 Factorial.py
@@ -4,39 +4,39 @@ x = input("Please enter a number: ")
 b = int(x)
 
 def factorial(n):
+    
+    
+    c = 0
+    times = []
+    for i in range(n,0,-1):
+        #Generates of a list of every number before n.
+        times.append(i)
+    '''
+    for k in range(0,len(times)):
+        if times[k] % 5 == 0:
+            c = c + 1
+    '''
 
-    cN = n
+    for m in range(1,len(times)-1):
+        #Causes every element of the list to times with each other.
+        times[0] = times[0] * times[m]
 
-    bN = [n]
 
-    while cN > 0:
-        print (str(cN-1))
-        cN = cN - 1
-        bN.append(cN)
-        
-
-    acceptables = []
-
-    i = 100
-    while i > 0:
-        a = i * 5
-        i = i - 1
-        acceptables.append(a)
-        print (str(acceptables))
-
-        
+    ans = str(times[0])
     count = 0
-    if acceptables in bN:
-            count = count + 1
-    for pos in bN:
-        bN[pos] = 0
-        if acceptables in bN:
-            count = count + 1
 
+    for c in range(len(ans)-1,0,-1):
+        #Checks how many zeros are at the end.
+        if ans[c] == "0":
+            count = count + 1
         else:
             break
+        
+    print (ans)
+    return str(n) + " Has " + str(count) + " Trailing Zero's"
 
-    print (str(n) + " Has " + str(count) + " Zero's at the end")
+    
+        
         
 
 
@@ -45,6 +45,6 @@ def factorial(n):
 
 
 
-factorial(b)
+print ( factorial(b))
 
 
